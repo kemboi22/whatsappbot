19470b005fa55035144824f2feb5e1da0b14c84a
kemboi22 <kemboielvis22@gmail.xom>
2023-03-15 15:33:00 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module example.com/m
+
+go 1.19
+
+require (
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/mattn/go-colorable v0.1.2 // indirect
+	github.com/mattn/go-isatty v0.0.8 // indirect
+	github.com/mattn/go-sqlite3 v1.14.16 // indirect
+	github.com/mdp/qrterminal v1.0.1 // indirect
+	github.com/mdp/qrterminal/v3 v3.0.0 // indirect
+	github.com/sashabaranov/go-openai v1.5.0 // indirect
+	go.mau.fi/libsignal v0.0.0-20221015105917-d970e7c3c9cf // indirect
+	go.mau.fi/whatsmeow v0.0.0-20221202110551-e067ee7293b0 // indirect
+	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a // indirect
+	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1 // indirect
+	google.golang.org/protobuf v1.29.0 // indirect
+	rsc.io/qr v0.2.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/mdp/qrterminal/v3"
+	openai "github.com/sashabaranov/go-openai"
+	"go.mau.fi/whatsmeow"
+	waProto "go.mau.fi/whatsmeow/binary/proto"
+	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
+	"go.mau.fi/whatsmeow/types/events"
+	waLog "go.mau.fi/whatsmeow/util/log"
+	"google.golang.org/protobuf/proto"
+)
+
+var errotMessage = ""
+
+type MyClient struct {
+	WAClient       *whatsmeow.Client
+	eventHandlerID uint32
+}
+
+func (mycli *MyClient) register() {
+	mycli.eventHandlerID = mycli.WAClient.AddEventHandler(mycli.eventHandler)
+}
+
+func SendMessage(message string) string {
+	client := openai.NewClient("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") //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
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleAssistant,
+					Content: message,
+				},
+			},
+		},
+	)
+	if err != nil {
+		fmt.Printf("ChatCompletion Error: %v", err)
+		errotMessage = err.Error()
+		return errotMessage
+	}
+
+	return resp.Choices[0].Message.Content
+
+}
+
+func (mycli *MyClient) eventHandler(evt interface{}) {
+	switch v := evt.(type) {
+	case *events.Message:
+		newMessage := v.Message
+		msg := newMessage.GetConversation()
+		fmt.Println("Message from:", v.Info.Sender.User, "->", msg)
+		if msg == "" {
+			return
+		}
+
+		chatGptResponse := SendMessage(msg)
+		response := &waProto.Message{Conversation: proto.String(string(chatGptResponse))}
+		fmt.Println("Response:", response)
+		userJid := types.NewJID(v.Info.Sender.User, types.DefaultUserServer)
+		owner := types.NewJID("254722790520", types.DefaultUserServer)
+		if errotMessage != "" {
+			response = &waProto.Message{Conversation: proto.String(string("An error occured! Please try again"))}
+
+		}
+		fixWhatsAppBot := &waProto.Message{Conversation: proto.String(string("You whatsapp bot has an error please look at it now \n" + errotMessage))}
+		mycli.WAClient.SendMessage(context.Background(), userJid, "", response)
+
+		if errotMessage != "" {
+			mycli.WAClient.SendMessage(context.Background(), owner, "", fixWhatsAppBot)
+		}
+
+	}
+}
+
+func main() {
+	dbLog := waLog.Stdout("Database", "DEBUG", true)
+	// Make sure you add appropriate DB connector imports, e.g. github.com/mattn/go-sqlite3 for SQLite
+	container, err := sqlstore.New("sqlite3", "file:examplestore.db?_foreign_keys=on", dbLog)
+	if err != nil {
+		panic(err)
+	}
+	// If you want multiple sessions, remember their JIDs and use .GetDevice(jid) or .GetAllDevices() instead.
+	deviceStore, err := container.GetFirstDevice()
+	if err != nil {
+		panic(err)
+	}
+	clientLog := waLog.Stdout("Client", "DEBUG", true)
+	client := whatsmeow.NewClient(deviceStore, clientLog)
+	// add the eventHandler
+	mycli := &MyClient{WAClient: client}
+	mycli.register()
+
+	if client.Store.ID == nil {
+		// No ID stored, new login
+		qrChan, _ := client.GetQRChannel(context.Background())
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+		for evt := range qrChan {
+			if evt.Event == "code" {
+				// Render the QR code here
+				// e.g. qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				// or just manually `echo 2@... | qrencode -t ansiutf8` in a terminal
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
+				//				fmt.Println("QR code:", evt.Code)
+			} else {
+				fmt.Println("Login event:", evt.Event)
+			}
+		}
+	} else {
+		// Already logged in, just connect
+		err = client.Connect()
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Listen to Ctrl+C (you can also do something else that prevents the program from exiting)
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	<-c
+
+	client.Disconnect()
+}
